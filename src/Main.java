1ecdf9c57c2db12d42ca1103a0aa69fb954d5fd9
AVMihel <dok2000@list.ru>
2025-02-07 09:00:10 +0500

    two

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
+
+        System.out.println(service.calculate(5_000));
+        System.out.println(service.calculate(15_000));
     }
 }
